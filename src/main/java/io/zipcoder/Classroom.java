46986d01a2d0f3c073423a820da6c361a8dc63aa
James Wilkinson <JamesW6179@gmail.com>
2020-02-20 21:58:14 -0500

    Stubbing before tests of Classroom

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,75 @@
 package io.zipcoder;
 
-public class Classroom {
+import java.util.ArrayList;
+import java.util.Map;
+
+public class Classroom
+{
+    ArrayList<Student> pupils;
+
+    public Classroom()
+    {
+        pupils = new ArrayList<>(30);
+    }
+
+    public Classroom(Integer classSize)
+    {
+        pupils = new ArrayList<>(classSize);
+    }
+
+    public Classroom(Student[] initialPupils)
+    {
+        pupils = new ArrayList<>(initialPupils.length);
+
+        for(Integer i = 0; i < initialPupils.length; i++)
+        {
+            pupils.add(i, initialPupils[i]);
+        }
+    }
+
+    public Student[] getStudents()
+    {
+        Student[] result = new Student[pupils.size()];
+
+        for(Integer i = 0; i < pupils.size(); i++)
+        {
+            result[i] = pupils.get(i);
+        }
+
+        return result;
+    }
+
+    public Double getAverageExamScore()
+    {
+        Double sum = 0.0;
+
+        for(Student pupil : pupils)
+        {
+            sum += getAverageExamScore();
+        }
+
+        return sum / pupils.size();
+    }
+
+    public void addStudent(Student newStudent)
+    {
+        pupils.add(newStudent);
+    }
+
+    public void removeStudent(String firstName, String lastName)
+    {
+
+    }
+
+    public Student[] getStudentsByScore()
+    {
+
+        return null;
+    }
+
+    public Map<Student, Character> getGradeBook()
+    {
+
+        return null;
+    }
 }
